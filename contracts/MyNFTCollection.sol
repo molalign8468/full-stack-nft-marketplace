227d64eef57fec99f4d18126d37189181c41709a
molalign8468 <molaligngetahun229@gmail.com>
2025-08-09 00:26:03 +0300

    Set up Hardhat and OpenZeppelin contracts

---
+++ contracts/MyNFTCollection.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.28;
+
+
+
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
