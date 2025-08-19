e793b574dfcd7bc06996e810df11ba89113c40ce
molalign8468 <molaligngetahun229@gmail.com>
2025-08-19 20:58:36 +0300

    Update NFT contract address

--- nft-marketplace-frontend/src/contracts/config.jsx
+++ nft-marketplace-frontend/src/contracts/config.jsx
@@ -3,6 +3,6 @@ import NFTCollectionABI from "./MyNFTCollection.json";
 
 export const marketplaceAddress = "0x1da9C613Db3383785bCd5bf22df46b55219dA36D";
 export const nftCollectionAddress =
-  "0x1ee7733004F9199307783eb4A991CD9adf3cd574";
+  "0x01B468456F3a17E024a0e02e5ac481953d487445";
 export const MAX_SUPPLY = 10000;
 export { MarketplaceABI, NFTCollectionABI };
