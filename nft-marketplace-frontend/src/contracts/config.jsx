5ad61f7ea84832efbae79c8d3aa34cf3a64d9ab2
molalign8468 <molaligngetahun229@gmail.com>
2025-10-17 23:44:46 +0300

    update failing Holsky contract address and point to Sepolia testnet for testing

--- nft-marketplace-frontend/src/contracts/config.jsx
+++ nft-marketplace-frontend/src/contracts/config.jsx
@@ -1,8 +1,8 @@
 import MarketplaceABI from "./NFTMarketplace.json";
 import NFTCollectionABI from "./MyNFTCollection.json";
 
-export const marketplaceAddress = "0x1da9C613Db3383785bCd5bf22df46b55219dA36D";
+export const marketplaceAddress = "0xccDdD4aE659295fD4A86340985ed3dd56eD2fe8C";
 export const nftCollectionAddress =
-  "0x01B468456F3a17E024a0e02e5ac481953d487445";
+  "0xD82088752d664D8Ce837D11Fd8FaD2c4e53dF061";
 export const MAX_SUPPLY = 10000;
 export { MarketplaceABI, NFTCollectionABI };
